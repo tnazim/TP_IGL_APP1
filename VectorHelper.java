51be570073b0deaae9df8ce76e8a41f6fb9ed3c3
tnazim <32871678+tnazim@users.noreply.github.com>
2017-10-17 16:15:12 +0100

    Update VectorHelper.java 2
    Adding the Bubble Sort and getSize methods.

--- VectorHelper.java
+++ VectorHelper.java
@@ -2,10 +2,12 @@ package com.company;
 import java.util.Random;
 
 public class VectorHelper {
+    int size;
     int vector[];
 
     /**Create and fill the vector with random numbers**/
     public VectorHelper(int size){
+        this.size=size;
         this.vector = new int[size];
         Random rand = new Random();
         for (int i=0;i<size;i++){
@@ -13,9 +15,34 @@ public class VectorHelper {
         }
     }
 
+    /**Bubble sort function**/
+    public void sort(){
+        int n=this.size;
+        int swapVar;
+        boolean repeat = true;
+        while (repeat)
+        {
+            repeat=false;
+            for (int i=1;i<n;i++)
+            {
+                if ((this.vector[i-1])>this.vector[i])
+                {
+                    swapVar=this.vector[i-1];
+                    this.vector[i-1]=this.vector[i];
+                    this.vector[i]=swapVar;
+                    repeat=true;
+                }
+            }
+        }
+    }
+
     /**Returns the vector**/
     public int[] getVector(){
         return this.vector;
     }
-
+    
+    /**Returns the size**/
+    public int getSize() {
+        return size;
+    }
 }
