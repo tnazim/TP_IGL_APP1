5377fa4b395bd9281e1759e6db506f1ebd77f587
tnazim <32871678+tnazim@users.noreply.github.com>
2017-10-17 15:57:44 +0100

    Update VectorHelper.java
    Adding constructor and getter.

--- VectorHelper.java
+++ VectorHelper.java
@@ -1,2 +1,21 @@
+package com.company;
+import java.util.Random;
+
 public class VectorHelper {
+    int vector[];
+
+    /**Create and fill the vector with random numbers**/
+    public VectorHelper(int size){
+        this.vector = new int[size];
+        Random rand = new Random();
+        for (int i=0;i<size;i++){
+            this.vector[i]=rand.nextInt(1000);
+        }
+    }
+
+    /**Returns the vector**/
+    public int[] getVector(){
+        return this.vector;
+    }
+
 }
